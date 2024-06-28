62710ef45bb2aecf6d0859cd22e096957b8bbed9
rmkirthy <rmkirthy@gmail.com>
2024-06-28 20:04:36 +0530

    up

--- 1_Basics/single_value_datatype.py
+++ 1_Basics/single_value_datatype.py
@@ -1,3 +1,18 @@
 a=10
 print(a)
 print(bool(a))
+
+b=-77
+print(type(b))
+print(bool(b))
+
+c=0 #The default value of integer is zero
+print(type(c))
+print(bool(c)) #false
+
+print("I am good boy \nHiii")
+print("Escaple \"hello\" ")
+
+d=-8.65
+print(type(d))
+print(bool(d))
